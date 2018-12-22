2fc94ff3a2b69523429c026b77ae7c08d6877555
Sudheendra Katikar <32903980+sudheendrakatikar@users.noreply.github.com>
2018-12-22 12:41:21 +0530

    added signin func

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -9,10 +9,23 @@ contract Identity {
 
     mapping (address => Keys) LinkedUser;
 
-    function createAccount( address _ethAddress, string memory _private_key, string memory _public_key) public {
+    function createAccount( address _ethAddress, string memory _private_key, string memory _public_key)
+    public {
         Keys memory newKeys;
         newKeys.private_key = _private_key;
         newKeys.public_key = _public_key;
         LinkedUser[_ethAddress] = newKeys;
-    }  
+    }
+    
+    function signIn(address ethAddress, string memory private_key, string memory public_key)
+    public
+    view
+    returns (bool) {
+        if (c.compareStrings(LinkedUser[ethAddress].private_key, private_key) &&
+            c.compareStrings(LinkedUser[ethAddress].public_key, public_key)) {
+            return true;
+        }
+        else
+            return false;
+    }
 }
