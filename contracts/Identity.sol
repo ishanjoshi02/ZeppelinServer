0d5043065a28976b89f65b70874b8ba45c50a647
Sudheendra Katikar <32903980+sudheendrakatikar@users.noreply.github.com>
2018-12-22 14:33:09 +0530

    Added private_key => ethAddress

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -1,22 +1,27 @@
 pragma solidity ^0.5.0;
+import "./Customer.sol";
+//pragma experimental ABIEncoderV2;
 
 contract Identity {
     
+    Customer c;
     struct Keys {
         string private_key;
         string public_key;
     }
 
     mapping (address => Keys) LinkedUser;
+    mapping (string => address) Account;
 
-    function createAccount( address _ethAddress, string memory _private_key, string memory _public_key)
+    function createAccount(address ethAddress, string memory _private_key, string memory _public_key)
     public {
         Keys memory newKeys;
         newKeys.private_key = _private_key;
         newKeys.public_key = _public_key;
-        LinkedUser[_ethAddress] = newKeys;
+        LinkedUser[ethAddress] = newKeys;
+        Account[_private_key] = ethAddress;
     }
-    
+
     function signIn(address ethAddress, string memory private_key, string memory public_key)
     public
     view
@@ -27,5 +32,22 @@ contract Identity {
         }
         else
             return false;
+    }
+
+    function getAddress(string memory private_key)
+    public
+    view
+    returns (address) {
+        return Account[private_key];
+    }
+
+    function compareStrings(string memory one, string memory two)
+    public
+    pure
+    returns (bool)
+    {
+
+        return keccak256(abi.encodePacked(one)) == keccak256(abi.encodePacked(two));
+
     }
 }
